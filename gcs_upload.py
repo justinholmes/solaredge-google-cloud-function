b70f960811eb8a49657d0bc6c31bea79f3e07eb4
Justin Holmes <justin@nascency.co.uk>
2019-05-01 23:30:15 +0100

    Added gzip

--- gcs_upload.py
+++ gcs_upload.py
@@ -1,10 +1,13 @@
+import zlib
+
 from google.cloud import storage
 import os
 
 client = storage.Client(project=os.environ.get('GCP_PROJECT'))
 bucket = client.get_bucket(os.environ.get('gcs_bucket'))
-
+compressobj = zlib.compressobj(9, zlib.DEFLATED, 31)
 
 def upload(date, blob_data):
     blob = bucket.blob("ingestion/solaredge/{}/data.json".format(date))
-    blob.upload_from_string(blob_data)
+    blob.content_encoding = 'gzip'
+    blob.upload_from_string(compressobj.compress(blob_data) + compressobj.flush())
